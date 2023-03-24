3e57bdd93c46089a3663006d3900c57fccaf7ae2
capellandiana <capellandiana@yahoo.com>
2023-03-24 14:18:02 -0400

    finishing hard

--- src/App.js
+++ src/App.js
@@ -4,36 +4,56 @@ import React from 'react';
 import BasicInfo from './BasicInfo';
 
 class App extends React.Component {
-constructor(props) {
-  super(props)
-  this.state = {person: {
-    name: 'Diana',
-    number: 704840,
-    dob: 'july 30 1997'
-  }}
+  constructor(props) {
+    super(props)
+    this.state = {
+      person: [
+        {
+          name: 'Diana',
+          number: 704840,
+          dob: 'july 30 1997'
+        },
+        {
+          name: 'Andres',
+          number: 784097384,
+          dob: 'july 9 2000'
+        },
+        {
+          name: 'Junior',
+          number: 7045673456,
+          dob: 'March 29 2002'
+        }
+      ]
+    }
 
-}
-render() {
-  return (
-        <div> 
+    }
+    render() {
+      return (
+        <div>
           <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <BasicInfo person={this.state.person} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <BasicInfo person={this.state.person} />
+            <ul>
+          {this.state.person.map(person => (
+            <li key={person.name}>
+              <BasicInfo person={person} />
+            </li>
+          ))}
+        </ul>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer" >
+              Learn React
+            </a>
+          </header>
         </div>
-  )
-};
-}
+      )
+    };
+  }
 
 export default App;
