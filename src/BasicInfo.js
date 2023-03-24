425d5927d00c4655e9d0993720168d607514cf6c
capellandiana <capellandiana@yahoo.com>
2023-03-24 11:58:50 -0400

    whoops forgot something

--- src/BasicInfo.js
+++ src/BasicInfo.js
@@ -1,11 +1,12 @@
 import React from 'react'
+
 function BasicInfo(props) {
     return (
       <div>
         <h2>Basic Information</h2>
         <p>Name: {props.person.name}</p>
-        <p>Age: {props.person.age}</p>
-        <p>Email: {props.person.email}</p>
+        <p>Age: {props.person.number}</p>
+        <p>Email: {props.person.dob}</p>
       </div>
     );
   }
